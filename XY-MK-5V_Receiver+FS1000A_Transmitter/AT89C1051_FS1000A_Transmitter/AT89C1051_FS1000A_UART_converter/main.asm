6e3ba80f0617e74b6ca963885501f1ce36d265ff
AssmoFun <bor1andrey@gmail.com>
2020-08-25 13:57:56 +0300

    Change not UTF symbols in main.asm
    Change not UTF symbols in main.asm

--- XY-MK-5V_Receiver+FS1000A_Transmitter/AT89C1051_FS1000A_Transmitter/AT89C1051_FS1000A_UART_converter/main.asm
+++ XY-MK-5V_Receiver+FS1000A_Transmitter/AT89C1051_FS1000A_Transmitter/AT89C1051_FS1000A_UART_converter/main.asm
@@ -5,7 +5,7 @@ include	'Libs\soft_uart_lib\soft_uart_extern.inc'
 ;===============================================================================
 main_idata	segment idata
 rseg		main_idata
-iseg		at	5h                                                              ; Stack = 11 bytes
+iseg		at	5h							; Stack = 11 bytes
 Stack:
 ;===============================================================================
 main_code	segment code
@@ -27,31 +27,31 @@ Init:
 	_init_stack
 	_clear_RAM
 ;== setup timers & interrupts for AT89C1051 ====================================
-;	orl		TMOD,	#00000001b	  ; ?imer0 mode
-	;					 ||||
-	;					 ||++------	?imer0 mode (01 - 16-bit, 10 - 8-bit with auto-reload, 11 - not available, functions the same as 01)
-	;					 |+--------	?/?0: 0 - timer, 1 - counter
-	;					 +---------	Gate_?0: 0-control by TR0, 1-control by TR0 & INT0
-
-;	orl		TCON,	#00000101b	  ; ?imer0 control
+;	orl		TMOD,		#00000001b	; Timer0 mode
+	;				     ||||
+	;				     ||++-------- Timer0 mode (01 - 16-bit, 10 - 8-bit with auto-reload, 11 - not available, functions the same as 01)
+	;				     |+---------- T/C0: 0 - timer, 1 - counter
+	;				     +----------- Gate_T0: 0-control by TR0, 1-control by TR0 & INT0
+	
+;	orl		TCON,		#00000101b	; Timer0 control
 	;				   ||||||
-	;				   |||||+------	IT0 - INT0 type: 0 - by level, 1 - by front
-	;				   ||||+-------	IE0 - request INT0 (if IT0=1, autoclear in heandler)
-	;				   |||+--------	IT1 - INT1 type: 0 - by level, 1 - by front
-	;				   ||+---------	IE1 - request INT1 (if IT1=1, autoclear in heandler)
-	;				   |+----------	TR0 - ?imer0 start
-	;				   +-----------	TF0 - Timer0 overflow flag
-
-;	orl		IE,		#10000010b	  ; Interrupts
+	;				   |||||+-------- IT0 - INT0 type: 0 - by level, 1 - by front
+	;				   ||||+--------- IE0 - request INT0 (if IT0=1, autoclear in heandler)
+	;				   |||+---------- IT1 - INT1 type: 0 - by level, 1 - by front
+	;				   ||+----------- IE1 - request INT1 (if IT1=1, autoclear in heandler)
+	;				   |+------------ TR0 - Timer0 start
+	;				   +------------- TF0 - Timer0 overflow flag
+	
+;	orl		IE,		#10000010b	; Interrupts
 	;				 ||||||||
-	;				 |||||||+------	EX0 - INT0 interrupt enable
-	;				 ||||||+-------	??0 - Timer0 interrupt enable
-	;				 |||||+-------- EX1 - INT1 interrupt enable
-	;				 ||||+--------- ??1 - reserv, not used
-	;				 |||+---------- ES  - reserv, not used
-	;				 ||+----------- ET2 - reserv, not used
-	;				 |+------------ reserv, not used
-	;				 +------------- ?? - global interrupts enable
+	;				 |||||||+-------- EX0 - INT0 interrupt enable
+	;				 ||||||+--------- ET0 - Timer0 interrupt enable
+	;				 |||||+---------- EX1 - INT1 interrupt enable
+	;				 ||||+----------- ET1 - reserv, not used
+	;				 |||+------------ ES  - reserv, not used
+	;				 ||+------------- ET2 - reserv, not used
+	;				 |+-------------- reserv, not used
+	;				 +--------------- IE - global interrupts enable
 ;===============================================================================
 Main:
 	_get_soft_uart		fs1000a_buff,soft_uart_bytecount
